c7c5d5a556c3ee2195e8318d65c65b3c644f8a89
handsome9ice <keresifon.ekpo@gmail.com>
2018-08-24 12:23:15 +0100

    pages addition from local

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+var responses;
+
+function responses() {
+    
+  var bounce = prompt("Are we there now?!");
+   
+    if (bounce === "yes") {
+        alert("woooo hooo!");
+    }else{
+        alert("bullocks!");
+    }
+}
+
